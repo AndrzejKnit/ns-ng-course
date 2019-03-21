fda0b8564735cf3479e5a4500d00b9f3985a6455
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-03-21 15:03:30 +0100

    Lesson 65: using the ListView

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { ItemEventData } from 'tns-core-modules/ui/list-view';
 
 @Component({
     selector: 'ns-current-challenge',
@@ -9,4 +10,7 @@ import { Component, Input } from "@angular/core";
 export class CurrentChallengeComponent {
   @Input() challenges: string[] = [];
 
+  onItemTap(args: ItemEventData) {
+      console.log(args);
+  }
 }
