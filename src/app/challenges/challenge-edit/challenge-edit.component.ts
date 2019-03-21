f2ee9fcfb8304cb7f8d34d7ce1f115ed3c296f71
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-03-21 14:03:59 +0100

    Lesson 62: Passing Data Between Components

--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'ns-challenge-edit',
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
   moduleId: module.id,
 })
 export class ChallengeEditComponent {
+   @Output() input = new EventEmitter<string>();
     challengeDescription = '';
 
     onSetChallenge() {
        // this.currentChallenge = this.challengeDescription;
+       this.input.emit(this.challengeDescription);
     }
 
 }
