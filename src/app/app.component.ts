8027ff831d4828c304405dd231e2b1ffbf6d059e
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-03-25 17:52:48 +0100

    Lesson 92: Displaying the Sidedrawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { UIService } from "./shared/ui/ui.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
     activeChallenge = '';
+    private drawerSub: Subscription;
+
+    constructor(private uiService: UIService) {}
+
+    ngOnInit() {
+      this.drawerSub =  this.uiService.drawerState.subscribe(()=> {
+          console.log('Toggle working!');
+      });
+    }
 
     onChallengeInput(challengeDescription: string) {
         this.activeChallenge = challengeDescription;
         console.log('onChallengeInput: ', challengeDescription);
+    }
+
+    ngOnDestroy() {
+        if (this.drawerSub) {
+            this.drawerSub.unsubscribe();
+        }
     }
  }
