7604bac09065c0be06b047c4b973b235d1992832
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-03 08:48:28 +0200

    Lesson 119: Styling Today's Challenge

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -8,7 +8,7 @@ declare var android: any;
 @Component({
     selector: 'ns-current-challenge',
     templateUrl: './current-challenge.component.html',
-    styleUrls: ['./current-challenge.component.css'],
+    styleUrls: ['./current-challenge.component.scss'],
     moduleId: module.id
 })
 export class CurrentChallengeComponent {
