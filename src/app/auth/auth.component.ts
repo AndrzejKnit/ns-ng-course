ff1f7780a4ce76f603fc24383df233954a96eda7
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-05 15:59:58 +0200

    Lesson 141: Switching Between Login & Signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -15,6 +15,7 @@ export class AuthComponent implements OnInit {
     form: FormGroup;
     emailControlIsValid = true;
     passwordControlIsValid = true;
+    isLogin = true;
     @ViewChild('passwordEl') passwordEl: ElementRef<TextField>
     @ViewChild('emailEl') emailEl: ElementRef<TextField>
 
@@ -47,9 +48,21 @@ export class AuthComponent implements OnInit {
       this.emailEl.nativeElement.focus();
       this.passwordEl.nativeElement.focus();
       this.passwordEl.nativeElement.dismissSoftInput();
+
+      if (!this.form.valid) {
+          return;
+      }
+
       const email = this.form.get('email').value;
       const password = this.form.get('password').value;
-      console.log(email, password);
+      this.form.reset();
+      this.emailControlIsValid = true;
+      this.passwordControlIsValid = true;
+      if (this.isLogin) {
+          console.log('Logging in...');
+      } else {
+          console.log('Signing up...');
+      }
   }
 
   onDone() {
@@ -57,4 +70,8 @@ export class AuthComponent implements OnInit {
       this.passwordEl.nativeElement.focus();
       this.passwordEl.nativeElement.dismissSoftInput();
   }
-}
+
+  onSwitch() {
+    this.isLogin = !this.isLogin;
+  }
+ }
