672ca89e4e3637ecff0505eb5da5a7bfab6fea14
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-05 15:08:48 +0200

    Lesson 139: Adding Validation & Reading Form Values

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { TextField } from 'tns-core-modules/ui/text-field';
 
 @Component({
   selector: 'ns-auth',
@@ -12,7 +15,8 @@ export class AuthComponent implements OnInit {
     form: FormGroup;
     emailControlIsValid = true;
     passwordControlIsValid = true;
-
+    @ViewChild('passwordEl') passwordEl: ElementRef<TextField>
+    @ViewChild('emailEl') emailEl: ElementRef<TextField>
 
   constructor(private router: RouterExtensions) { }
 
@@ -30,5 +34,14 @@ export class AuthComponent implements OnInit {
 
   onSignin() {
     this.router.navigate(['/today'], {clearHistory: true});
+  }
+
+  onSubmit() {
+      this.emailEl.nativeElement.focus();
+      this.passwordEl.nativeElement.focus();
+      this.passwordEl.nativeElement.dismissSoftInput();
+      const email = this.form.get('email').value;
+      const password = this.form.get('password').value;
+      console.log(email, password);
   }
 }
