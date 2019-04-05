2781d364f5e45a2a13f91dfecb37bdb6e8ccb5e3
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-05 12:57:15 +0200

    Lesson 133: Configuring Form Inputs

--- src/app/challenges/challenge-edit/challenge-edit.module.ts
+++ src/app/challenges/challenge-edit/challenge-edit.module.ts
@@ -3,11 +3,13 @@ import { ChallengeEditComponent } from "./challenge-edit.component";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { SharedModule } from "~/app/shared/shared.module";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 @NgModule({
     declarations: [ChallengeEditComponent],
     imports: [
         NativeScriptCommonModule,
+        NativeScriptFormsModule,
         //NativeScriptRouterModule,
         NativeScriptRouterModule.forChild([
             { path: '', component: ChallengeEditComponent}]),
