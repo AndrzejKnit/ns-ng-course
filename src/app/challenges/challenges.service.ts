11c4d0ce84fdf31a8feb1a396e7ee3df9476e7fc
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-09 13:31:12 +0200

    Lesson 147: Adding the Challenge & Day Models

--- src/app/challenges/challenges.service.ts
+++ src/app/challenges/challenges.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { Challenge } from "./challenge.model";
 
 @Injectable({ providedIn: 'root' })
 export class ChallengeService {
-    private _currentChallenge = new BehaviorSubject<{}>(null);
+    private _currentChallenge = new BehaviorSubject<Challenge>(null);
 }
