a4cc66928fb917b7dbcc3d5851466ab757e1b446
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-16 12:36:34 +0200

    Lesson 180: Managing the User in the Auth Service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { catchError, tap } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { throwError, BehaviorSubject } from "rxjs";
 import { alert } from 'tns-core-modules/ui/dialogs';
 import { User } from "./user.model";
+import { Challenge } from "../challenges/challenge.model";
 
 const FIREBASE_API_KEY = 'AIzaSyB6OvfualQIwAJHZHwL01Hk9W4cs8KVhfU';
 
@@ -14,31 +15,55 @@ interface AuthResponse {
     refreshToken: string;
     expiresIn: string
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
+    private _user = new BehaviorSubject<User>(null);
+
     constructor(private http: HttpClient) {}
+
+    get user() {
+        return this._user.asObservable();
+    }
+
      signUp(email: string, password: string) {
       return  this.http.post<AuthResponse>(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${FIREBASE_API_KEY}`
         , {email: email, password: password, returnSecureToken: true}).pipe(catchError(errorRes => {
             this.handleError(errorRes.error.error.message);
             return throwError(errorRes);
         }),
+
         tap(resData => {
             if (resData && resData.idToken) {
-                const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
-                const user = new User(email, resData.localId, resData.idToken, expirationDate);
+                this.handleLogin(email, resData.idToken, resData.localId, parseInt(resData.expiresIn));
             }
         })
         );
     }
     login(email: string, password: string) {
-        return this.http.post(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${FIREBASE_API_KEY}`
+        return this.http.post<AuthResponse>(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${FIREBASE_API_KEY}`
         , {email: email, password: password, returnSecureToken: true}).pipe(catchError(errorRes => {
             this.handleError(errorRes.error.error.message);
             return throwError(errorRes);
-        }));
+        }),
+        tap(resData => {
+            if (resData && resData.idToken) {
+                this.handleLogin(email, resData.idToken, resData.localId, parseInt(resData.expiresIn));
+            }
+        })
+        );
+    }
+
+    private handleLogin(email: string, token: string, userId: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+                const user = new User(
+                    email,
+                    userId,
+                    token,
+                    expirationDate);
+                this._user.next(user);
     }
 
     private handleError(errorMessage: string) {
