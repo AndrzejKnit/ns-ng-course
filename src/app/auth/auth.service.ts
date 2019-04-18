389e11f21d3e04f2c33a15041a016988613198ef
Andrzej Knitter <andrzej.knitter@gmail.com>
2019-04-18 16:52:24 +0200

    Lesson 187: Adding an Authentication Guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -90,7 +90,7 @@ export class AuthService {
     if (this.tokenExpirationTimer) {
         clearTimeout(this.tokenExpirationTimer);
     }
-    this.router.navigate(['/'], { clearHistory: true });
+    this.router.navigate(['/auth'], { clearHistory: true });
   }
 
   autoLogin() {
@@ -114,7 +114,6 @@ export class AuthService {
     if (loadedUser.isAuth) {
       this._user.next(loadedUser);
       this.autoLogout(loadedUser.timeToExpiry);
-      this.router.navigate(['/challenges'], { clearHistory: true });
       return of(true);
     }
     return of(false);
